823e1eaa1903450c88aa3c91f1332452a7998162
Flash-Memory <coolboymusic64@gmail.com>
2026-05-09 23:37:43 -0600

    hello world

---
+++ hyperstar-synth1/hyperstar-synth1/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hyperstar-synth1
+//
+//  Created by Cole Nelson on 5/9/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
